4217622b2bfc24eb62431bf046f5773347f02c23
Niklas Bergius <niklas@bergius.org>
2013-07-05 18:33:18 +0200

    Yes?

--- manage.sh
+++ manage.sh
@@ -6,17 +6,32 @@
 # 3) invalid port
 # 4) sudo required
 # 5) already installed
+# 6) not installed or broken installation
+# 7) instance already exists
+# 8) instance not enabled
 
 set -e
 
 echoerr() { echo "$@" 1>&2; }
-ensuresudo() {
-  if [[ ! `whoami` == 'root' ]]; then
-    echoerr Sudo, please.
-    exit 4
+exists() { ls -U $1 &> /dev/null; }
+installed() { test -x /etc/init.d/redis-landlord; }
+ensureinstalled() {
+  installed
+  if [[ ! $? ]]; then
+    echoerr Landlord not installed.
+    echo "Run \"`basename \"$0\"` install\"."
+    exit 6
   fi
 }
-exists() { ls -U $1 &> /dev/null; }
+instexists() { test -x /etc/init.d/redis-available/$1; }
+ensureinstexists() {
+  instexists $1
+  if [[ ! $? ]]; then
+    echoerr $id does not exist.
+    echo "Run \"`basename \"$0\"` setup $id <port>\" to create."
+    exit 9
+  fi
+}
 
 cmds=("install" "uninstall" "setup" "disable" "enable" "delete")
 cmd=$1
@@ -26,17 +41,29 @@ enabled=/etc/init.d/redis-enabled
 bootstrapper=/etc/init.d/redis-tenants
 confdir=/etc/redis
 
+scriptpath="`dirname \"$0\"`"
+scriptpath="`( cd \"$scriptpath\" && pwd )`/`basename \"$0\"`"
+
+if [[ ! `whoami` == 'root' ]]; then
+  echoerr This script needs to be run as a superuser.
+  echo For a password-free experience, add the following
+  echo to your sudoers file:
+  echo
+  echo %redis ALL=NOPASSWORD: $scriptpath
+  exit 4
+fi
+
 if [[ ! " ${cmds[@]} " =~ " ${cmd//[^a-z]/} " ]]; then
   old_ifs=$IFS
   IFS=','
-  echo "Usage: $0 <command> [arg [arg...]]"
+  echo "Usage: `basename \"$0\"` <command> [arg [arg]]"
   cmds="${cmds[*]}"
   echo "Commands:"
   echo "  setup <id> <port>  Sets up an instance."
   echo "  disable <id>       Disables an instance"
   echo "  enable <id>        Enables a previously disabled instance."
   echo "  delete <id>        Deletes an instance."
-  echo "  install            Installs init scripts and Redis"
+  echo "  install [force]    Installs init scripts and Redis"
   echo "                     configuration. Use with care."
   echo "  uninstall [hard]   Uninstalls init scripts, and optionally"
   echo "                     everything else (if hard). Use with"
@@ -50,6 +77,13 @@ fi
 case $cmd in
 # setup <id> <port>
 'setup')
+  ensureinstalled
+  if [[ $(instexists) ]]; then
+    echoerr $id already exists.
+    echo "Run \"`basename \"$0\"` enable\" to enable."
+    exit 7
+  fi
+
   port=$3
 
   [[ ! $port =~ ^[1-9][0-9]*$ ]] && echoerr Invalid port. && exit 3
@@ -67,24 +101,38 @@ case $cmd in
   echo OK
 
   echo Running bootstrapper ...
-  sudo $bootstrapper start
+  $bootstrapper start
   ;;
 
 'disable'*)
+  ensureinstalled
+  ensureinstexists $id
+
+  if [[ ! -x "$enabled/$id" ]]; then
+    echoerr $id not enabled.
+    exit 8
+  fi
+
   echo Stopping and disabling instance $id ...
-  sudo /etc/init.d/redis-enabled/$id stop && rm $enabled/$id
+  /etc/init.d/redis-enabled/$id stop && rm $enabled/$id
   ;;
 
 'enable'*)
+  ensureinstalled
+  ensureinstexists $id
+
   echo -n "Enabling instance $id ..."
-  ln -s $available/$id $enabled/$id
+  [[ ! -x "$available/$id" ]] && ln -s $available/$id $enabled/$id
   echo OK
 
   echo Running bootstrapper ...
-  sudo $bootstrapper start
+  $bootstrapper start
   ;;
 
 'delete'*)
+  ensureinstalled
+  ensureinstexists $id
+
   ./$0 disable $id
   echo -n "Deleting instance $id ... "
   rm $available/$id
@@ -92,8 +140,6 @@ case $cmd in
   ;;
 
 'install'*)
-  ensuresudo
-
   if [[ -x /etc/init.d/redis-landlord && $id != 'force' ]]; then
     echoerr Landlord already installed, you probably want to uninstall first.
     exit 5
@@ -135,15 +181,18 @@ case $cmd in
   for dir in /etc/init.d/redis-{available,enabled}; do
     [[ ! -d $dir ]] && mkdir $dir
   done
-  chown redis:redis /etc/init.d/redis-{available,enabled}
-  chmod g+w /etc/redis /etc/init.d/redis-{available,enabled} /var/run/redis
+  # chown redis:redis /etc/redis /etc/init.d/redis-{available,enabled}
+  # chmod g+w /etc/redis /etc/init.d/redis-{available,enabled} /var/run/redis
   echo OK
 
   /etc/init.d/redis-landlord start
   ;;
 
 'uninstall'*)
-  ensuresudo
+  if [[ ! -x /etc/init.d/redis-landlord ]]; then
+    echoerr Landlord not installed or installation broken.
+    exit 6
+  fi
 
   if [[ $id == 'hard' ]]; then
     echo 'Removing all traces of tenancy ...'
